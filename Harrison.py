1d3be47554d2026295ef0277de9ae43afe4aebe1
harrisonCassar - Laptop <harrison.cassar@gmail.com>
2018-10-23 20:36:18 -0700

    Updated for LED Test

--- Harrison.py
+++ Harrison.py
@@ -1,1 +1,13 @@
-print("Hello world!")
+#http://pi4j.com/pins/model-3b-rev1.html
+#
+
+from gpiozero import LED
+from time import sleep
+
+led = LED(17)
+
+while True:
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
